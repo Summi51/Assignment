c5b2c25d532e8784222d964d189c0a345c78017b
Summi51 <inayatsamreen1411@gmail.com>
2025-01-11 05:35:49 +0530

    done

--- Frontend/github-frontend/src/utils/api.js
+++ Frontend/github-frontend/src/utils/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://autonomizebackend.onrender.com",
+  baseURL: "//https://autonomizebackend.onrender.com",
 });
 
 export default instance;
+
+
+
